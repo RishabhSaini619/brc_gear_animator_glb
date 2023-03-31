951d227c41f5fdda048f47ba50e3039505e0dde5
Rishabh Saini <rishabhsaini619@gmail.com>
2023-03-31 20:20:14 +0530

    start

---
+++ app/src/main/java/com/kotlin/brc/gear_animator_glb/MainActivity.kt
@@ -0,0 +1,11 @@
+package com.kotlin.brc.gear_animator_glb
+
+import android.support.v7.app.AppCompatActivity
+import android.os.Bundle
+
+class MainActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+    }
+}
