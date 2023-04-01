3316aa3d39509e22a57dc6543bd5b80311ac9fc6
Rishabh Saini <rishabhsaini619@gmail.com>
2023-04-01 14:37:27 +0530

    layout

--- app/src/main/java/com/kotlin/brc/gear_animator_glb/MainActivity.kt
+++ app/src/main/java/com/kotlin/brc/gear_animator_glb/MainActivity.kt
@@ -1,11 +1,11 @@
 package com.kotlin.brc.gear_animator_glb
 
-import android.support.v7.app.AppCompatActivity
 import android.os.Bundle
+import androidx.appcompat.app.AppCompatActivity
 
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_main)
+        setContentView(R.layout.file_layout)
     }
 }
