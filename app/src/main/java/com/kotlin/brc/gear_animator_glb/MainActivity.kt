61bdc818fe53064ad1483fdf2048aedb80c89d48
Rishabh Saini <rishabhsaini619@gmail.com>
2023-04-01 20:33:07 +0530

    setupEnvironment

--- app/src/main/java/com/kotlin/brc/gear_animator_glb/MainActivity.kt
+++ app/src/main/java/com/kotlin/brc/gear_animator_glb/MainActivity.kt
@@ -27,8 +27,85 @@ import java.net.URLConnection
 import java.nio.ByteBuffer
 
 class MainActivity : AppCompatActivity() {
+    companion object{
+        init {
+            Utils.init()
+        }
+        val view = modelViewer.view
+        private val fm = FlamModel()
+        private lateinit var choreographer: Choreographer
+        private lateinit var doubleTapDetector: GestureDetector
+//        private val frameScheduler = FrameCallback()
+//        private val doubleTapListener = DoubleTapListener()
+        private val viewerContent = AutomationEngine.ViewerContent()
+        private lateinit var bottomSheetBehavior: BottomSheetBehavior<*>
+        private lateinit var bottomDrawerSheet: ConstraintLayout
+        private lateinit var modelViewer: ModelViewer
+        private lateinit var surfaceView: SurfaceView
+        private lateinit var avatarUrlField: EditText
+        private lateinit var animationUrlField: EditText
+        private lateinit var glbAnimatorButton: MaterialButton
+        private var isClicked=false
+    }
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.file_layout)
+        choreographer = Choreographer.getInstance()
+        surfaceView = findViewById(R.id.view_surface)
+
+        bottomDrawerSheet = findViewById(R.id.bottom_drawer_sheet)
+        bottomSheetBehavior = BottomSheetBehavior.from(bottomDrawerSheet)
+
+        setupEnvironment()
     }
+    private fun setupEnvironment() {
+
+        // on mobile, better use lower quality color buffer
+        view.renderQuality = view.renderQuality.apply {
+            hdrColorBuffer = View.QualityLevel.MEDIUM
+        }
+
+        // dynamic resolution often helps a lot
+        view.dynamicResolutionOptions = view.dynamicResolutionOptions.apply {
+            enabled = true
+            quality = View.QualityLevel.MEDIUM
+        }
+
+        // MSAA is needed with dynamic resolution MEDIUM
+        view.multiSampleAntiAliasingOptions = view.multiSampleAntiAliasingOptions.apply {
+            enabled = true
+        }
+
+        // FXAA is pretty cheap and helps a lot
+        view.antiAliasing = View.AntiAliasing.FXAA
+
+        // ambient occlusion is the cheapest effect that adds a lot of quality
+        view.ambientOcclusionOptions = view.ambientOcclusionOptions.apply {
+            enabled = true
+        }
+
+        // bloom is pretty expensive but adds a fair amount of realism
+        view.bloomOptions = view.bloomOptions.apply {
+            enabled = true
+        }
+
+        doubleTapDetector = GestureDetector(applicationContext, doubleTapListener)
+        modelViewer = ModelViewer(surfaceView, engine = Engine.create())
+        viewerContent.view = modelViewer.view
+        viewerContent.sunlight = modelViewer.light
+        viewerContent.lightManager = modelViewer.engine.lightManager
+        viewerContent.scene = modelViewer.scene
+        viewerContent.renderer = modelViewer.renderer
+
+        surfaceView.setOnTouchListener { _, event ->
+            modelViewer.onTouchEvent(event)
+            doubleTapDetector.onTouchEvent(event)
+            true
+        }
+
+        setUpEventListener()
+        createIndirectLight()
+        createDefaultRenderables()
+    }
+
 }
